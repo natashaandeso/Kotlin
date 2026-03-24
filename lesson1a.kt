534e525fa0c599378e8cdd25780f3ccaf2be9cb9
andesonatasha <andesonatasha@gmail.com>
2026-03-24 14:40:28 +0300

    kotlin files

---
+++ lesson1a.kt
@@ -0,0 +1,19 @@
+fun main(){
+    //  A variable is a container that stores data/values
+    // In kotlin we have two main types of initializers i.e var and val
+    // Val => immutable (unchangeable) but var => mutable (changeable)
+
+    val name = "Natasha" // string -> a series of character inside of quotes
+    val number1 = 5 // Interger -> whole number
+    val number2 = 40.6 // Double -> Number with a decimal
+    var letter = "k" // Char -> It is a single letter of the alphabet
+    val isRegistered = true // Boolean -> It evaluates to either true or false
+
+
+    println(name)
+    println(number1)
+    println(number2)
+    println(letter)
+    println(isRegistered)
+    
+}
---
+++ lesson1b.kt
@@ -0,0 +1,18 @@
+fun main(){
+    // Below we declare variables and then we state their data types
+    val number1 : Int = 5
+    val number2 : Double = 3.142
+    val letter : Char = 'B'
+    val paidLoan : Boolean = false
+    val name : String = "James"
+
+    // print out the output
+    println(number1)
+    println(number2)
+    println(letter)
+    println(paidLoan)
+    println(name)
+}
+
+// create a file call it assignment1.kt => create an addition and substraction programs inside of the same file
+// Research on conditional staements in kotlin
